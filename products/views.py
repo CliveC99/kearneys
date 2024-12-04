5af28f26075a82f2da73da947baa118198b9ae66
CliveC99 <119447148+CliveC99@users.noreply.github.com>
2024-12-04 02:40:20 +0000

    Added sorting and icon to get to top of the page

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
+from django.db.models.functions import Lower
 from .models import Product, Category
 
 # Create your views here.
@@ -21,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -54,11 +56,10 @@ def all_products(request):
     return render(request, 'products/products.html', context)
 
 
-
 def product_detail(request, product_id):
-    """ A view to show individual products details """
+    """ A view to show individual product details """
 
-    product = get_object_or_404(Product, pk=product_id) 
+    product = get_object_or_404(Product, pk=product_id)
 
     context = {
         'product': product,
